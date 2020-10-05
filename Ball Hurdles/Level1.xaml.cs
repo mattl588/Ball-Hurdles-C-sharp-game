b0c1a11d02c58526403e820bd79fd5c4bc514e76
Matt Lacaire <71798926+mattl588@users.noreply.github.com>
2020-10-04 23:03:00 -0700

    Add files via upload

---
+++ Ball Hurdles/Level1.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Ball_Hurdles
+{
+    /// <summary>
+    /// Interaction logic for Level1.xaml
+    /// </summary>
+    public partial class Level1 : Window 
+    {
+        DispatcherTimer levelTimer = new DispatcherTimer();
+        DispatcherTimer secondTimer = new DispatcherTimer();
+        readonly int rollspeed = 1;
+        double height;
+        int timeremaining = 16;
+        bool isHit = false;
+        public Level1()
+        {
+            InitializeComponent();
+            levelTimer.Tick += Mechanics;
+            levelTimer.Interval = TimeSpan.FromMilliseconds(20);
+            secondTimer.Tick += Countdown;
+            secondTimer.Interval = TimeSpan.FromMilliseconds(1000);
+            Initialize();
+        }
+        private void Mechanics(object sender, EventArgs e)
+        {
+            Canvas.SetLeft(GameBall, Canvas.GetLeft(GameBall) + rollspeed);
+        }
+        private void Countdown(object sender, EventArgs e)
+        {
+            timeremaining--;
+            timeleft.Content = timeremaining;
+            if (timeremaining <= 0)
+            {
+                if (Canvas.GetTop(GameBall) <= 40) //height of the hurdle. This makes it so that if the time is out and the hurdle is surpassed in height, the game is won.
+                {
+                    timeleft.Content = "You Win!!";
+                    if (timeremaining <= -3)
+                    {
+                        this.Close();
+                    }
+                }
+                else
+                {
+                    timeleft.Content = "Better luck next time!";
+                    if (timeremaining <= -3) //roundabout, but for some reason, Thread.Sleep(3000) completely bricks the screen. Not sure why- I'll look into it.
+                    {
+                        this.Close();
+                    }
+                }
+            }
+        }
+        private void KeyIsDown(object sender, KeyEventArgs e) 
+        {
+            if (e.Key == Key.Space)
+            {
+                while (!isHit) //isHit logic makes it so that hitting the key once causes one change in height. Otherswise, if inserted in a loop, simply holding down the key would win the game. No fun!
+                {
+                    height += 0.07; //for some odd reason, adding integer values of "height" causes the ball to launch higher by a large amount. I find it unclear why, but using doubles is better and actually makes the game challenging. 
+                    isHit = true;
+                    Canvas.SetTop(GameBall, Canvas.GetTop(GameBall) - height); 
+                }
+            }
+        }
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            isHit = false;
+        }
+        private void Initialize()
+        {
+            GameCanvas.Focus(); 
+            levelTimer.Start();
+            secondTimer.Start();
+        }
+    }
+}
---
+++ Ball Hurdles/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading; //Lots of libraries I didn't use. Doesn't change the program to include them though, does it? 
+
+namespace Ball_Hurdles
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window //this class is primarily for the main menu and the fun stuff with messing around in it. If "yup!" is hit, a new instance of 
+    {
+        DispatcherTimer gameTimer = new DispatcherTimer();
+        int gravity = 8;
+        public MainWindow()
+        {
+            InitializeComponent();
+            gameTimer.Tick += MainEventTimer;
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+            Initialize();
+        }
+        private void MainEventTimer(object sender, EventArgs e)
+        {
+            Canvas.SetTop(Ball, Canvas.GetTop(Ball) + gravity);
+            if (Canvas.GetTop(Ball) >= 290)
+            {
+                gravity = -8;
+            }
+            if (Canvas.GetTop(Ball) <= 40)
+            {
+                gravity = 8;
+            }
+
+        }
+        private void KeyIsDown(object sender, KeyEventArgs e) //made entirely for the sake of learning. Just makes the ball go up and down for no reason :)
+        {
+            if (e.Key == Key.Space)
+            {
+                gravity = -8;
+            }
+        }
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            gravity = 8;
+        }
+        private void Initialize()
+        {
+            MyCanvas.Focus();
+            Canvas.SetTop(Ball, 0);
+            gameTimer.Start();
+        }
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            Level1 level = new Level1(); //I instantiate a case of "Level1", which is on another cs file. 
+            level.Show();
+            this.Close();
+        }
+    }
+}
